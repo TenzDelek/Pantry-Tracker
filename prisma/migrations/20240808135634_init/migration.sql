087075230fa24855d259560e029d053eb265c74b
TenzDelek <tibetdelek@gmail.com>
2024-08-09 16:23:09 +0530

    delete migrate file

--- prisma/migrations/20240808135634_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "pantry" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-
-    CONSTRAINT "pantry_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20240808141031_added/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `updated` to the `pantry` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "pantry" ADD COLUMN     "updated" TIMESTAMP(3) NOT NULL;
